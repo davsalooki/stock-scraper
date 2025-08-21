de77836e8787494b1b57dbe27831aaa4bbcebf92
David Le <davsalooki@gmail.com>
2025-08-21 16:30:47 +1000

    add remove stock warning disabled trigger

--- app/src/routes/watchlist/components/RemoveStockWarning.svelte
+++ app/src/routes/watchlist/components/RemoveStockWarning.svelte
@@ -1,22 +1,25 @@
 <script lang="ts">
 	import * as AlertDialog from '$lib/components/ui/alert-dialog/index.js';
-	import { Trigger } from '$lib/components/ui/dialog';
-	const { trigger, state } = $props();
+	import Button from '$lib/components/ui/button/button.svelte';
+	const { selectedStockCode } = $props();
 </script>
 
 <AlertDialog.Root>
-	<AlertDialog.Trigger>Open</AlertDialog.Trigger>
+	<AlertDialog.Trigger disabled={selectedStockCode == null}>
+		<Button disabled={selectedStockCode == null}>Delete</Button>
+	</AlertDialog.Trigger>
 	<AlertDialog.Content>
 		<AlertDialog.Header>
 			<AlertDialog.Title>Are you absolutely sure?</AlertDialog.Title>
 			<AlertDialog.Description>
-				This action cannot be undone. This will permanently delete your account and remove your data
-				from our servers.
+				You are deleting {selectedStockCode} from your watchlist.
 			</AlertDialog.Description>
 		</AlertDialog.Header>
 		<AlertDialog.Footer>
-			<AlertDialog.Cancel>Cancel</AlertDialog.Cancel>
-			<AlertDialog.Action>Continue</AlertDialog.Action>
+			<form>
+				<AlertDialog.Cancel>Cancel</AlertDialog.Cancel>
+				<AlertDialog.Action>Confirm</AlertDialog.Action>
+			</form>
 		</AlertDialog.Footer>
 	</AlertDialog.Content>
 </AlertDialog.Root>
