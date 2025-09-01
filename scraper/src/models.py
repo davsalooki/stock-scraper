5e01931bfa92bbaec530ba59b4afb2de91d9ac93
David Le <davsalooki@gmail.com>
2025-09-01 17:33:35 +1000

    Added Pydantic models'

---
+++ scraper/src/models.py
@@ -0,0 +1,25 @@
+from typing import Self
+from pydantic import BaseModel, model_validator
+
+from .config import TICKER_LIST
+
+class StockInfo(BaseModel):
+    financials: dict[str, str | int | float]
+    earnings_roe_chart: str
+
+class Ticker(BaseModel):
+    exchange_code: str
+    stock_code: str
+
+    def __repr__(self):
+        return f"{self.exchange_code}:{self.stock_code}"
+
+    @model_validator(mode="after")
+    def is_a_stock(self) -> Self:
+        if f"{self.exchange_code}:{self.stock_code}" not in TICKER_LIST:
+            raise ValueError("Invalid stock code")
+        return self
+
+class Stock(BaseModel):
+    ticker: Ticker
+    info: StockInfo
