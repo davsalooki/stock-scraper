93c3f0fc0837c31d85c0073b6166d594ca3812f9
David Le <davsalooki@gmail.com>
2025-09-15 15:46:11 +1000

    Moved external API types to a separate file

---
+++ app/src/routes/watchlist/types.d.ts
@@ -0,0 +1,10 @@
+export type StockSummary = {
+    exchange_code: string;
+    ticker_symbol: string;
+    name: string;
+}
+
+export type StockIdentifier = {
+    exchange_code: string;
+    ticker_symbol: string;
+}
