b7ee94e235612069205faf1d4e4f67703495e20b
David Le <davsalooki@gmail.com>
2025-09-15 11:35:19 +1000

    Added DELETE action to delete stock from watchlist

--- app/src/routes/watchlist/stocks/[exchange]/[stock]/+page.server.ts
+++ app/src/routes/watchlist/stocks/[exchange]/[stock]/+page.server.ts
@@ -1,3 +1,5 @@
+import type { Actions } from "./$types";
+
 export async function load({ params }) {
     const exchange = params.exchange;
     const stock = params.stock;
@@ -13,4 +15,21 @@ async function getStockFinancials(exchange: string, stock: string) {
     const res = await fetch(`${url}/stocks/${exchange}/${stock}/financials`);
     const data = await res.json();
     return data;
+}
+
+export const actions: Actions = {
+	delete: async ({ request }) => {
+		const formData = await request.formData();
+		const exchange = formData.get('exchange');
+		const ticker = formData.get('ticker');
+
+		// logic to delete the stock from the watchlist
+        const res = await fetch(`${url}/watchlist/stocks/${exchange}/${ticker}`, {
+            method: 'DELETE'
+        });
+        if (!res.ok) {
+            return { success: false, message: 'Failed to delete stock from watchlist' };
+        }
+        return { success: true, message: 'Stock deleted from watchlist' };
+	}
 }
